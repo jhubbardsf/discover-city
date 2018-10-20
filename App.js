f607ebbbfa7fed2b66af9a0e9d5b743f00e81807
Josh Hubbard <jhubbardsf@gmail.com>
2018-10-20 08:10:00 -0400

    Adding load state to button.

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { Button, FormLabel, FormInput, FormValidationMessage } from 'react-native-elements'
+import { Button, FormLabel, FormInput, FormValidationMessage } from 'react-native-elements';
+import opencage from 'opencage-api-client';
 
 export default class App extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			coords: '',
-			address: ''
+			coords: '31.79261,35.21785',
+			address: '',
+			geocoding: false
 		}
 	}
 	
@@ -18,13 +20,26 @@ export default class App extends React.Component {
 				<FormLabel style={{padding: 20}}>Enter Co-ordinates (latitude, longitude)</FormLabel>
 				<FormInput containerStyle={styles.input}
 					onChangeText={(coords) => this.setState({coords})}/>
-				<Button title='Geocode'
-								buttonStyle={styles.button}
-								color='#FFFFFF'
+				<Button
+					loading={this.state.geocoding}
+					buttonStyle={{backgroundColor: "blue"}}
+					onPress={this.reverseGeocode}
+					title={this.state.geocoding ? "Geocoding..." : "Geocode"}
+					color="#ffffff"
 				/>
 				<Text style={styles.result}>{this.state.address}</Text>
 			</View>
 		);
+	}
+	
+	reverseGeocode = () => {
+		const key = 'e6c003e249714bf5b9991bfd2ac96d15';
+		this.setState({geocoding: true});
+		opencage.geocode({key, q: this.state.coords}).then(response => {
+			result = response.results[0];
+			console.log(result.formatted);
+			this.setState({address: result.formatted, geocoding: false});
+		});
 	}
 }
 
