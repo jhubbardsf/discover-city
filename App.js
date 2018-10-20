562abf39a951d47f013124b5bc3607fae6086f18
Josh Hubbard <jhubbardsf@gmail.com>
2018-10-20 07:11:55 -0400

    Added basic react-native-elements.

--- App.js
+++ App.js
@@ -1,24 +1,44 @@
 import React from 'react';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
+import { Button, FormLabel, FormInput, FormValidationMessage } from 'react-native-elements'
 
 export default class App extends React.Component {
 	render() {
 		return (
 			<View style={styles.container}>
-				<Text>Josh's Discover City</Text>
-				<TextInput />
-				<Button title='Geocode' />
-				<Text>Human understandable address</Text>
+				<Text style={styles.title}>Josh's City Discovery</Text>
+				<FormLabel style={{padding: 20}}>Enter Co-ordinates (latitude, longitude)</FormLabel>
+				<FormInput containerStyle={styles.input} />
+				<Button title='Geocode'
+								buttonStyle={styles.button}
+								color='#FFFFFF'
+				/>
+				<Text style={styles.result}>Human understandable address</Text>
 			</View>
 		);
 	}
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		marginTop: 100,
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	title: {
+		fontSize: 30,
+	},
+	input: {
+		borderColor: 'gray',
+		borderWidth: 1,
+		marginBottom: 20,
+		width: '80%',
+	},
+	button: {
+		backgroundColor: '#1E88E5',
+		width: 200,
+	},
+	result: {
+		padding: 10,
+	}
 });
