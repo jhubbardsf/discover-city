aa4a9b4e14aebd50eb666c6fad5e235ab225bddd
Josh Hubbard <jhubbardsf@gmail.com>
2018-10-20 07:14:42 -0400

    Make the app take in values and display them

--- App.js
+++ App.js
@@ -3,17 +3,26 @@ import { StyleSheet, Text, View } from 'react-native';
 import { Button, FormLabel, FormInput, FormValidationMessage } from 'react-native-elements'
 
 export default class App extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			coords: '',
+			address: ''
+		}
+	}
+	
 	render() {
 		return (
 			<View style={styles.container}>
 				<Text style={styles.title}>Josh's City Discovery</Text>
 				<FormLabel style={{padding: 20}}>Enter Co-ordinates (latitude, longitude)</FormLabel>
-				<FormInput containerStyle={styles.input} />
+				<FormInput containerStyle={styles.input}
+					onChangeText={(coords) => this.setState({coords})}/>
 				<Button title='Geocode'
 								buttonStyle={styles.button}
 								color='#FFFFFF'
 				/>
-				<Text style={styles.result}>Human understandable address</Text>
+				<Text style={styles.result}>{this.state.address}</Text>
 			</View>
 		);
 	}
